9de812bbb26e5e2e38de6de21202ff8c721bc4f0
lucasGlla <lukinhapontes@outlook.com>
2025-08-24 20:25:38 -0300

    Move games on list

---
+++ src/main/java/com/devsuperior/dslist/dto/ReplacementDTO.java
@@ -0,0 +1,23 @@
+package com.devsuperior.dslist.dto;
+
+public class ReplacementDTO {
+	
+	private Integer sourceIndex;
+	private Integer destiantionIndex;
+	
+	public Integer getSourceIndex() {
+		return sourceIndex;
+	}
+	public void setSourceIndex(Integer sourceIndex) {
+		this.sourceIndex = sourceIndex;
+	}
+	public Integer getDestiantionIndex() {
+		return destiantionIndex;
+	}
+	public void setDestiantionIndex(Integer destiantionIndex) {
+		this.destiantionIndex = destiantionIndex;
+	}
+	
+	
+
+}
